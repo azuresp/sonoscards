b4deeb08106837faacdb2547254b0064dfab6fb5
Stephen Phillips <stephen.phillips@pobox.com>
2017-04-22 22:28:07 -0700

    tweaks

--- iotcam.py
+++ iotcam.py
@@ -16,7 +16,7 @@ FULLSCREEN = not DEBUG
 if not DEBUG:
     RESOLUTION = (640, 480)
 else:
-	RESOLUTION = (480, 270)
+	RESOLUTION = (1024, 768)
 
 # Initialise Raspberry Pi camera
 camera = PiCamera()
@@ -32,17 +32,12 @@ time.sleep(0.1)
 print("PiCamera ready")
 
 # Initialise OpenCV window
-if FULLSCREEN:
-	cv2.namedWindow("#iothack15", cv2.WND_PROP_FULLSCREEN)
-	cv2.setWindowProperty("#iothack15", cv2.WND_PROP_FULLSCREEN, cv2.cv.CV_WINDOW_FULLSCREEN)
-else:
-	cv2.namedWindow("#iothack15")
+cv2.namedWindow("#iothack15")
 
 print("OpenCV version: %s" % (cv2.__version__))
 print("Press q to exit ...")
 
 scanner = zbar.Scanner()
-# scanner.parse_config('enable')
 
 # Capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
@@ -51,14 +46,9 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
     # raw detection code
     gray = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY, dstCn=0)
-    pil = Image.fromarray(gray)
-    width, height = pil.size
-    raw = pil.tobytes()
-    print("raw length= %s" % len(raw))
+    # print("raw length= %s" % len(raw))
 
     # create a reader
-    #image = zbar.Image(width, height, 'Y800', raw)
-
     image = scanner.scan(gray)
 
     # extract results
