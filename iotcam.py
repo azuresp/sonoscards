c5d8361e0bd54130c7e36b5164627397b7512df2
Stephen Phillips <azuresp@pobox.com>
2017-05-14 18:50:05 -0700

    tweak camera

--- iotcam.py
+++ iotcam.py
@@ -13,8 +13,7 @@ if len(sys.argv) > 1:
 	DEBUG = sys.argv[-1] == 'DEBUG'
 
 # Configuration options
-FULLSCREEN = not DEBUG
-if not DEBUG:
+if DEBUG:
     RESOLUTION = (640, 480)
 else:
 	RESOLUTION = (1024, 768)
@@ -22,10 +21,7 @@ else:
 # Initialise Raspberry Pi camera
 camera = PiCamera()
 camera.resolution = RESOLUTION
-#camera.framerate = 10
-#camera.vflip = True
-#camera.hflip = True
-#camera.color_effects = (128, 128)
+
 # set up stream buffer
 rawCapture = PiRGBArray(camera, size=RESOLUTION)
 # allow camera to warm up
@@ -33,7 +29,7 @@ time.sleep(0.1)
 print("PiCamera ready")
 
 # Initialise OpenCV window
-cv2.namedWindow("#iothack15")
+# cv2.namedWindow("#iothack15")
 
 print("OpenCV version: %s" % (cv2.__version__))
 print("Press q to exit ...")
@@ -47,19 +43,17 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
     # raw detection code
     gray = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY, dstCn=0)
-    # print("raw length= %s" % len(raw))
 
     # create a reader
     image = scanner.scan(gray)
 
     # extract results
     for symbol in image:
-        # do something useful with results
         print('decoded', symbol.type, 'symbol', '"%s"' % symbol.data)
         sonosclient.parse(symbol.data.decode('utf-8'))
 
     # show the frame
-    cv2.imshow("#iothack15", output)
+    # cv2.imshow("#iothack15", output)
 
     # clear stream for next frame
     rawCapture.truncate(0)
