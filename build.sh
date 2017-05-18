f56882eaa313220d7377541b75bb66dc710eabb4
Stephen Phillips <stephen.phillips@pobox.com>
2017-05-17 21:30:08 -0700

    got scanner working

--- build.sh
+++ build.sh
@@ -1,10 +1,3 @@
-Remove-Item export\*
-"***** ZBAR build"
-docker build -t rpi-zbar rpi-zbar
-"***** Scanner add-ons"
+docker build --network=host -t rpi-zbar rpi-zbar
 docker build -t sonos-scanner scanner
-"***** Exporting Scanner"
-docker image save sonos-scanner:latest -o export\sonos-scanner.tar
-"***** Doing controller (this is fast!)"
 docker build -t sonos-controller controller
-docker image save sonos-controller:latest -o export\sonos-controller.tar
